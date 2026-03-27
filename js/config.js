da1db9c35848f9062a01188b9aae9461c022bc6d
LaxmanSidhu <laxman2025.sawant@gmail.com>
2026-03-27 13:59:42 +0530

    Fix thumbnail paths in SHEETS configuration
    Updated thumbnail paths to remove leading '../' in SHEETS object.

--- js/config.js
+++ js/config.js
@@ -2,9 +2,9 @@
 const BASE_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRHEji0MCywpiCqKw_aMNA2woVKKaU7VnBnbajgUvfU3Z2rIbHQUZghAZ867mXlpyHC-pw9TiSRmLSu/pub?output=csv&gid=";
 
 const SHEETS = {
-  siddhesh: {name: "Siddhesh", gid: "0", thumbnail: "../assets/sidhu.jpeg", description: "My personal reading collection"},
-  pratiksha: {name: "Pratiksha", gid: "1145659502", thumbnail: "../assets/pratu.jpeg", description: "Pratiksha's curated book list"},
-  dhanashri: { name: "Dhanashri", gid: "679217924", thumbnail: "../assets/dhanu.jpeg", description: "Dhanashri's reading journey"},
+  siddhesh: {name: "Siddhesh", gid: "0", thumbnail: "assets/sidhu.jpeg", description: "My personal reading collection"},
+  pratiksha: {name: "Pratiksha", gid: "1145659502", thumbnail: "assets/pratu.jpeg", description: "Pratiksha's curated book list"},
+  dhanashri: { name: "Dhanashri", gid: "679217924", thumbnail: "assets/dhanu.jpeg", description: "Dhanashri's reading journey"},
   read_list: { name: "Read List", gid: "798459865", thumbnail: "https://images.theconversation.com/files/45159/original/rptgtpxd-1396254731.jpg?ixlib=rb-4.1.0&q=45&auto=format&w=1356&h=668&fit=crop", description: "Our upcoming books to read" }
 };
 
